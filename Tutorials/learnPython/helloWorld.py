42666c93aa548387951ca80a14be550b400e857d
Griffin White <griffinowhite@gmail.com>
2020-07-07 14:17:54 -0500

    Completed the Hello World program and its assignment.

---
+++ Tutorials/learnPython/helloWorld.py
@@ -0,0 +1,15 @@
+# Hashtags denote comments.
+
+# You can print simple stings in Python with the 'print' statement. Enclose strings in quotes.
+print("test")
+print("hello world\nHELLO WORLD\nnew lines\n\n\n\n\nHello World")
+
+#Instead of curly braces, Python uses indentation for blocks. Both 'tab' and four spaces are acceptable.
+
+# Setting a variable equal to 3.
+x = 3
+# An if statement that is true whenever 'x' equals 3.
+if x == 3:
+    # Indented over.
+    # Printing out 'x equals 3' when the if statement is true.
+    print("x equals 3")
---
+++ Tutorials/learnPython/helloWorldExercise.py
@@ -0,0 +1,5 @@
+# Exercise
+# Use the "print" command to print the line "Hello, World!".
+
+# Usig the 'print' statement to print out 'Hello, World!'.
+print("Hello, World!")
