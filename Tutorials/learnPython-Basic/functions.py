7edba99be219dbfcf35b883b45a33a2919a9dd73
Griffin White <griffinowhite@gmail.com>
2020-07-16 14:12:07 -0500

    Began the functions program.

---
+++ Tutorials/learnPython-Basic/functions.py
@@ -0,0 +1,31 @@
+# Functions let you divde your code into useful blocks. Functions can make your code more readible and allow your to reuse code within your program.
+# You follow the same indentation rules when defining functions.
+# Functions are defined using the keyword 'def' followed by the function's name.
+def testFunc():
+    print ("This is being printed by the function\n")
+
+# You then call to the function using its name, followed by parenthesis.
+testFunc()
+
+# Functions can also be sent arguments whenever they are called. You enter the arguments into the paranthese following the name of the function when it is called.
+def sendFunc(name, age):
+    print ("Your name is %s, and you are %d years old\n" %(name, age))
+# Sending the variables.
+sendFunc ("Andrew", 35)
+
+# How Do You Call Functions in Python: You type the function's name followed by '()'. Place any required variables in the parenthesis.
+
+def basicFunc():
+    print("No arguments were sent to this function.")
+
+def numFunc(num1, num2):
+    print("Two numbers were sent to this function: %d and %d." %(num1, num2))
+
+def stringFunc(string1, string2):
+    print("Two strings were sent to this function: '%s' and '%s'." %(string1, string2))
+
+# Calling to the functions.
+basicFunc()
+# Calling to the functions and sending them arguments.
+numFunc(42,3)
+stringFunc("Hello", "There")
